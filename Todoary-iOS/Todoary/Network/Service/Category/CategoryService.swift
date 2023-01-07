a8ed44095359b6a74a810aab46dd47d9e67669f6
Ssooya <bsy24660@naver.com>
2023-01-07 16:40:10 +0900

    #350: CategoryService 구현

--- Todoary-iOS/Todoary/Network/Service/Category/CategoryService.swift
+++ Todoary-iOS/Todoary/Network/Service/Category/CategoryService.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+class CategoryService: BaseService{
+    static let shared = CategoryService()
+    private override init() {}
+}
+
+extension CategoryService {
+    
+    func getCategories(completion: @escaping (NetworkResult<Any>) -> Void){
+        requestObject(CategoryRouter.getCategories, type: [CategoryModel].self, decodingMode: .model, completion: completion)
+    }
+    
+    func generateCategory(request: CategoryMakeInput, completion: @escaping (NetworkResult<Any>) -> Void){
+        requestObjectWithEmptyResponse(CategoryRouter.postCategory(requeset: request), completion: completion)
+    }
+    
+    func modifyCategory(id: Int, request: CategoryMakeInput, completion: @escaping (NetworkResult<Any>) -> Void){
+        requestObjectWithEmptyResponse(CategoryRouter.fetchCategory(id: id, request: request), completion: completion)
+    }
+    
+    func deleteCategory(id: Int, completion: @escaping (NetworkResult<Any>) -> Void){
+        requestObjectWithEmptyResponse(CategoryRouter.deleteCategory(id: id), completion: completion)
+    }
+}
