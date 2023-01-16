1560600aa77283ea4a2e81facd35786e50a3645c
chaeyoung103 <thdcodud103@naver.com>
2023-01-16 12:54:09 +0900

    /#360: 다이어리 스티커, 투두생성수정 router, service 추가

--- Todoary-iOS/Todoary/Network/Service/Todo/TodoRouter.swift
+++ Todoary-iOS/Todoary/Network/Service/Todo/TodoRouter.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+import Alamofire
+
+enum TodoRouter{
+    case postTodo(requeset: TodoSettingInput)
+    case patchTodo(id: Int, request: TodoModifyInput)
+}
+
+
+extension TodoRouter: BaseRouter{
+    
+    var path: String{
+        switch self{
+        case .postTodo:                         return HTTPMethodURL.POST.sticker
+            //sticker 아니고 todo 따로 추가 해야함
+        case .patchTodo(let id, _):             return HTTPMethodURL.PATCH.todo + "/\(id)"
+
+        }
+    }
+    
+    var method: HTTPMethod{
+        switch self{
+        case .postTodo:                         return .post
+        case .patchTodo:                        return .patch
+        }
+    }
+    
+    var parameters: RequestParams {
+        switch self{
+        case .postTodo(let request):            return .requestBody(request)
+        case .patchTodo(_, let request):        return .requestBody(request)
+        }
+    }
+    
+    var header: HeaderType{
+        switch self{
+        default:    return .withToken
+        }
+    }
+}
