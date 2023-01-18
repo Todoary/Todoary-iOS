284322347ea5fe4db02d4872c654fb6a9589bc80
Ssooya <bsy24660@naver.com>
2023-01-18 12:47:20 +0900

    refactor: TodoGetDataManager 파일 제거 및 Todoary용 dateformatter 사용 위한 TodoaryDateFormatter 파일 생성

--- Todoary-iOS/Todoary/Global/Source/TodoaryDateFormatter.swift
+++ Todoary-iOS/Todoary/Global/Source/TodoaryDateFormatter.swift
@@ -1,15 +1,13 @@
 //
-//  TodoGetDataManager.swift
+//  TodoaryDateFormatter.swift
 //  Todoary
 //
-//  Created by 박지윤 on 2022/07/27.
+//  Created by 박지윤 on 2023/01/18.
 //
 
 import Foundation
-import Alamofire
 
 struct ConvertDate{
-    
     var year: Int?
     var month: Int?
     var date: String?
