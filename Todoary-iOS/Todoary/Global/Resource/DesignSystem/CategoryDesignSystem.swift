1abb4f126d1c7aee0f75b966547963a3105f60eb
SSOOYA <bsy24660@naver.com>
2023-01-29 16:26:38 +0900

    #389: DesignSystem 구축 위한 파일 추가

---
+++ Todoary-iOS/Todoary/Global/Resource/DesignSystem/CategoryDesignSystem.swift
@@ -0,0 +1,8 @@
+//
+//  CategoryDesignSystem.swift
+//  Todoary
+//
+//  Created by 박소윤 on 2023/01/29.
+//
+
+import Foundation
---
+++ Todoary-iOS/Todoary/Global/Resource/DesignSystem/TypoDesignSystem.swift
@@ -0,0 +1,8 @@
+//
+//  TypoDesignSystem.swift
+//  Todoary
+//
+//  Created by 박소윤 on 2023/01/29.
+//
+
+import Foundation
