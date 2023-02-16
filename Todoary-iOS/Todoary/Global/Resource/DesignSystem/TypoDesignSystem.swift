e2c57f7a780111c2be212f6859b0c831286a4db5
SSOOYA <bsy24660@naver.com>
2023-02-16 16:40:24 +0900

    #389: Typo Style 추가 케이스 정리

--- Todoary-iOS/Todoary/Global/Resource/DesignSystem/TypoDesignSystem.swift
+++ Todoary-iOS/Todoary/Global/Resource/DesignSystem/TypoDesignSystem.swift
@@ -27,10 +27,13 @@ struct LabelDescription {
 
 enum TypoStyle: Int, CaseIterable {
     
+    case regualar12              // 12pt / medium / -0.08 / 16
+    
     case medium12_2             // 12pt / medium / 0.24 / 14.4
     case medium12_3             // 12pt / medium / 0.36 / 14.4
     case medium13               // 13pt / medium / -0.26 / 15.6
-    case medium14               // 14pt / medium / 0.28 / 16.8
+    case medium14_16               // 14pt / medium / 0.28 / 16.8
+    case medium14_21               // 14pt / medium / 0.28 / 21
     case medium15               // 15pt / medium / 0.3 / 18
     case medium16               // 16pt / medium / 0.32 / 19.2
     
@@ -39,23 +42,29 @@ enum TypoStyle: Int, CaseIterable {
     case bold14                 // 14pt / bold / 0.28 / 16.8
     case bold15_18              // 15pt / bold / 0.3 / 18
     case bold15_22              // 15pt / bold / 0.3 / 22
-    case bold16                 // 16pt / bold / 0.32 / 19.2
+    case bold16_19                 // 16pt / bold / 0.32 / 19.2
+    case bold17                 // 17pt / bold / -0.41 / 22
     case bold18                 // 18pt / bold / ? / 21.6
     case bold18_2               // 18pt / bold / 0.36 / 21.6
     
     case semibold14             // 14pt / semibold / 0.28 / 16.8
+    case semibold17             // 17pt / semibold / -0.41 / 22
     case semibold18             // 18pt / semibold / 0.36 / 21.6
 
+    case extrabold12            // 12pt / extrabold / 0.24 / 14.4
     case extrabold13            // 13pt / extrabold / 0.26 / 15.6
 }
 
 extension TypoStyle {
     private var fontDescription: FontDescription {
         switch self {
+        case .regualar12:               return FontDescription(font: .Regular, size: 12)
+            
         case .medium12_2:               return FontDescription(font: .Medium, size: 12)
         case .medium12_3:               return FontDescription(font: .Medium, size: 12)
         case .medium13:                 return FontDescription(font: .Medium, size: 13)
-        case .medium14:                 return FontDescription(font: .Medium, size: 14)
+        case .medium14_16:                 return FontDescription(font: .Medium, size: 14)
+        case .medium14_21:                 return FontDescription(font: .Medium, size: 14)
         case .medium15:                 return FontDescription(font: .Medium, size: 15)
         case .medium16:                 return FontDescription(font: .Medium, size: 16)
         
@@ -66,24 +75,30 @@ extension TypoStyle {
         case .bold14:                   return FontDescription(font: .Bold, size: 14)
         case .bold15_18:                return FontDescription(font: .Bold, size: 15)
         case .bold15_22:                return FontDescription(font: .Bold, size: 15)
-        case .bold16:                   return FontDescription(font: .Bold, size: 16)
+        case .bold16_19:                return FontDescription(font: .Bold, size: 16)
+        case .bold17:                   return FontDescription(font: .Bold, size: 17)
         case .bold18:                   return FontDescription(font: .Bold, size: 18)
         case .bold18_2:                 return FontDescription(font: .Bold, size: 18)
             
             
         case .semibold14:               return FontDescription(font: .SemiBold, size: 14)
+        case .semibold17:               return FontDescription(font: .SemiBold, size: 17)
         case .semibold18:               return FontDescription(font: .SemiBold, size: 18)
         
+        case .extrabold12:              return FontDescription(font: .ExtraBold, size: 12)
         case .extrabold13:              return FontDescription(font: .ExtraBold, size: 13)
         }
     }
     
     public var labelDescription: LabelDescription {
         switch self {
+        case .regualar12:               return LabelDescription(kern: -0.08, lineHeight: 16)
+            
         case .medium12_2:               return LabelDescription(kern: 0.24, lineHeight: 14.4)
         case .medium12_3:               return LabelDescription(kern: 0.36, lineHeight: 14.4)
         case .medium13:                 return LabelDescription(kern: -0.26, lineHeight: 15.6)
-        case .medium14:                 return LabelDescription(kern: 0.28, lineHeight: 16.8)
+        case .medium14_16:                 return LabelDescription(kern: 0.28, lineHeight: 16.8)
+        case .medium14_21:                 return LabelDescription(kern: 0.28, lineHeight: 21)
         case .medium15:                 return LabelDescription(kern: 0.3, lineHeight: 18)
         case .medium16:                 return LabelDescription(kern: 0.32, lineHeight: 19.2)
         
@@ -92,13 +107,16 @@ extension TypoStyle {
         case .bold14:                   return LabelDescription(kern: 0.28, lineHeight: 16.8)
         case .bold15_18:                return LabelDescription(kern: 0.3, lineHeight: 18)
         case .bold15_22:                return LabelDescription(kern: 0.3, lineHeight: 22)
-        case .bold16:                   return LabelDescription(kern: 0.32, lineHeight: 19.2)
+        case .bold16_19:                return LabelDescription(kern: 0.32, lineHeight: 19.2)
+        case .bold17:                   return LabelDescription(kern: -0.41, lineHeight: 22)
         case .bold18:                   return LabelDescription(kern: 0, lineHeight: 21.6)
         case .bold18_2:                 return LabelDescription(kern: 0.36, lineHeight: 21.6)
         
         case .semibold14:               return LabelDescription(kern: 0.28, lineHeight: 16.8)
+        case .semibold17:               return LabelDescription(kern: 0.41, lineHeight: 22)
         case .semibold18:               return LabelDescription(kern: 0.36, lineHeight: 21.6)
         
+        case .extrabold12:              return LabelDescription(kern: 0.24, lineHeight: 14.4)
         case .extrabold13:              return LabelDescription(kern: 0.26, lineHeight: 15.6)
         }
     }
