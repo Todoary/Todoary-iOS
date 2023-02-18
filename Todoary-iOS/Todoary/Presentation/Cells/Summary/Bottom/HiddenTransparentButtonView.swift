a740a6b3d09bc09e96946909440202241b2f2806
SSOOYA <bsy24660@naver.com>
2023-02-18 15:34:27 +0900

    #399: HiddenLeftButtonView Base 클래스 적용으로 리팩토링

--- Todoary-iOS/Todoary/Presentation/Cells/Summary/Bottom/HiddenTransparentButtonView.swift
+++ Todoary-iOS/Todoary/Presentation/Cells/Summary/Bottom/HiddenTransparentButtonView.swift
@@ -40,8 +40,7 @@ class HiddenRightButtonView: BaseView {
     }
 }
 
-
-class HiddenLeftButtonView: UIView{
+class HiddenLeftButtonView: BaseView{
     
     lazy var pinButton = UIButton().then{
         $0.setImage(Image.pushPinBig, for: .normal)
@@ -61,49 +60,42 @@ class HiddenLeftButtonView: UIView{
         $0.backgroundColor = .transparent
     }
     
-    override init(frame: CGRect) {
-        
-        super.init(frame: frame)
-        
+    override func hierarchy() {
         self.addSubview(transparentView)
-    
         transparentView.addSubview(pinButton)
         transparentView.addSubview(alarmBtn)
         transparentView.addSubview(borderLine)
-        
-        self.snp.makeConstraints{ make in
-            make.height.equalTo(60)
-            make.width.equalTo(105) // 58
-        }
-
-        transparentView.snp.makeConstraints{ make in
-            make.leading.equalToSuperview()
-            make.trailing.equalToSuperview()
-            make.top.bottom.equalToSuperview()
-        }
-        
-        pinButton.snp.makeConstraints{ make in
-            make.width.height.equalTo(28)
-            make.leading.equalToSuperview().offset(18)
-            make.centerY.equalToSuperview()
-        }
-        
-        borderLine.snp.makeConstraints{ make in
-            make.width.height.equalTo(1)
-            make.top.equalToSuperview().offset(17)
-            make.bottom.equalToSuperview().offset(-15)
-            make.leading.equalTo(pinButton.snp.trailing).offset(10)
-        }
-        
-        alarmBtn.snp.makeConstraints{ make in
-            make.width.height.equalTo(28)
-            make.leading.equalTo(borderLine.snp.trailing).offset(11)
-            make.centerY.equalToSuperview()
-        }
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    override func layout() {
+        self.snp.makeConstraints{
+            $0.height.equalTo(60)
+            $0.width.equalTo(105) // 58
+        }
 
+        transparentView.snp.makeConstraints{
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            $0.top.bottom.equalToSuperview()
+        }
+        
+        pinButton.snp.makeConstraints{
+            $0.width.height.equalTo(28)
+            $0.leading.equalToSuperview().offset(18)
+            $0.centerY.equalToSuperview()
+        }
+        
+        borderLine.snp.makeConstraints{
+            $0.width.height.equalTo(1)
+            $0.top.equalToSuperview().offset(17)
+            $0.bottom.equalToSuperview().offset(-15)
+            $0.leading.equalTo(pinButton.snp.trailing).offset(10)
+        }
+        
+        alarmBtn.snp.makeConstraints{
+            $0.width.height.equalTo(28)
+            $0.leading.equalTo(borderLine.snp.trailing).offset(11)
+            $0.centerY.equalToSuperview()
+        }
+    }
 }
