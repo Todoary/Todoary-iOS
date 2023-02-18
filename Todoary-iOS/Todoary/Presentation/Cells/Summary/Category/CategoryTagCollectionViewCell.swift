c1e3db8a638768e341b019229997c91fdecb0922
SSOOYA <bsy24660@naver.com>
2023-02-18 15:28:59 +0900

    #399: CategoryTagCollectionViewCell cellIdentifier 제거 및 생성자 제거

--- Todoary-iOS/Todoary/Presentation/Cells/Summary/Category/CategoryTagCollectionViewCell.swift
+++ Todoary-iOS/Todoary/Presentation/Cells/Summary/Category/CategoryTagCollectionViewCell.swift
@@ -8,13 +8,11 @@
 import UIKit
 
 class CategoryTagCollectionViewCell: BaseCollectionViewCell{
-    
-    static let cellIdentifier = "CategoryTagCollectionViewCell"
-    
+
     private let tagLabel = CategoryTag.generateForDefault()
     
-    override init(frame: CGRect) {
-        super.init(frame: .zero)
+    override func prepareForReuse() {
+        tagLabel.initialize()
     }
     
     override func hierarchy() {
@@ -27,14 +25,6 @@ class CategoryTagCollectionViewCell: BaseCollectionViewCell{
         tagLabel.snp.makeConstraints{
             $0.top.leading.trailing.bottom.equalToSuperview()
         }
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func prepareForReuse() {
-        tagLabel.initialize()
     }
     
     func bindingData(title: String, color: Int){
