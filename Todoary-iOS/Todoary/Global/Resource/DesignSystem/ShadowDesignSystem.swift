1f34ec3c8b4e26190b23be60aa852601be7639f0
SSOOYA <bsy24660@naver.com>
2023-02-19 14:23:03 +0900

    style: ShadowView shadowOffset height 값 수정 (2 > 1)

--- Todoary-iOS/Todoary/Global/Resource/DesignSystem/ShadowDesignSystem.swift
+++ Todoary-iOS/Todoary/Global/Resource/DesignSystem/ShadowDesignSystem.swift
@@ -24,7 +24,7 @@ class ShadowView: BaseView{
         self.backgroundColor = .white
         self.layer.shadowRadius = 10.0
         self.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
-        self.layer.shadowOffset = CGSize(width: 0, height: 2)
+        self.layer.shadowOffset = CGSize(width: 0, height: 1) //2
         self.layer.shadowOpacity = 1
         self.layer.masksToBounds = false
         self.layer.cornerRadius = cornerRadius
