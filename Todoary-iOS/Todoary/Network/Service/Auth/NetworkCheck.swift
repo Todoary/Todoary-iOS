7be15754a349922a1c4866b01e470e1f3db6c03d
SSOOYA <bsy24660@naver.com>
2023-02-28 15:07:23 +0900

    #426: NetworkCheck 도메인 주소 변경

--- Todoary-iOS/Todoary/Network/Service/Auth/NetworkCheck.swift
+++ Todoary-iOS/Todoary/Network/Service/Auth/NetworkCheck.swift
@@ -13,7 +13,7 @@ class NetworkCheck{
     func networkCheck(){
         let fcmToken = FcmTokenRequestModel(fcmToken: "networkCheck")
         
-        AF.request("https://todoary.com/member/fcm_token", method: .patch, parameters: fcmToken,  encoder: JSONParameterEncoder.default , interceptor: Interceptor()).validate().responseDecodable(of: NetworkCheckModel.self) { response in
+        AF.request("https://\(Bundle.main.infoDictionary?["API-BASE-URL"] as? String ?? "")/member/fcm_token", method: .patch, parameters: fcmToken,  encoder: JSONParameterEncoder.default , interceptor: Interceptor()).validate().responseDecodable(of: NetworkCheckModel.self) { response in
             switch response.result {
             case .success(let result):
                 switch result.code {
