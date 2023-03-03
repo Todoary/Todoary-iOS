4aad17f392055493e1854f5aaf186be969321ec2
SSOOYA <bsy24660@naver.com>
2023-03-03 15:14:23 +0900

    #424: UserDefaultKey 파일 추가 및 프로퍼티 세팅

---
+++ Todoary-iOS/Todoary/Global/Source/UserDefault/UserDefaultKey.swift
@@ -0,0 +1,18 @@
+//
+//  UserDefaultKey.swift
+//  Todoary
+//
+//  Created by 박소윤 on 2023/03/03.
+//
+
+import Foundation
+
+struct UserDefaultKey{
+    static let fcmToken = "fcmToken"
+    static let isFirstTime = "isFirstTime"
+    static let refreshToken = "refreshToken"
+    static let defaultImg = "defaultImg"
+    static let emailCertificationCode = "emailCertificationCode" //기존 key 네이밍
+    static let hasAppPassword = "hasAppPassword"
+    static let appPassword = "appPassword" //기존 newPasswordArr
+}
