9e31d37af0f52787d276d77e2398cd48b9039918
SSOOYA <bsy24660@naver.com>
2023-03-03 15:15:32 +0900

    #424: UserManager 클래스 생성 및 UserDefaultKey에 따른 프로퍼티 세팅 > 임시로 String? 타입 설정

---
+++ Todoary-iOS/Todoary/Global/Source/UserDefault/UserManager.swift
@@ -0,0 +1,31 @@
+//
+//  UserManager.swift
+//  Todoary
+//
+//  Created by 박소윤 on 2023/03/03.
+//
+
+import Foundation
+
+class UserManager {
+    @UserDefault(key: UserDefaultKey.fcmToken, defaultValue: nil)
+    static var fcmToken: String?
+    
+    @UserDefault(key: UserDefaultKey.isFirstTime, defaultValue: nil)
+    static var isFirstTime: String?
+    
+    @UserDefault(key: UserDefaultKey.refreshToken, defaultValue: nil)
+    static var refreshToken: String?
+    
+    @UserDefault(key: UserDefaultKey.defaultImg, defaultValue: nil)
+    static var defaultImg: String?
+    
+    @UserDefault(key: UserDefaultKey.emailCertificationCode, defaultValue: nil)
+    static var emailCertificationCode: String?
+    
+    @UserDefault(key: UserDefaultKey.hasAppPassword, defaultValue: nil)
+    static var hasAppPassword: String?
+    
+    @UserDefault(key: UserDefaultKey.appPassword, defaultValue: nil)
+    static var appPassword: String?
+}
