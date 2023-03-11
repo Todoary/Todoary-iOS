6cc9d350a1f20adc1ddefbf5c418c9e685ff05db
SSOOYA <bsy24660@naver.com>
2023-03-11 14:53:17 +0900

    #439: AppleSignUpResultModel 서버 응답모델 변경으로 인한 isDeactivatedUser 프로퍼티 추가

--- Todoary-iOS/Todoary/Network/Entity/Auth/AppleSignUpResultModel.swift
+++ Todoary-iOS/Todoary/Network/Entity/Auth/AppleSignUpResultModel.swift
@@ -9,10 +9,11 @@ import Foundation
 
 struct AppleSignUpResultModel: Codable{
     let isNewUser: Bool
+    let isDeactivatedUser: Bool
     let name: String
     let email: String
     let provider: String
     let providerId: String
-    let token: Token
+    let token: Token //TODO: null로 바꿔야 할 수도
     let appleRefreshToken: String
 }
